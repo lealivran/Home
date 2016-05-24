e95f445c33803a6501bdef5b4a7792bc3a188d3a
lealivran <lealivran@msn.com>
2016-05-24 14:18:02 +0200

    connect bdd

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/home-dev'
+    uri: 'mongodb://admin:supdeweb@ds011893.mlab.com:11893/homesdw'
   },
 
   // Seed database on startup
---
+++ server/models/annonce.js
@@ -0,0 +1,17 @@
+// import mongoose from "mongoose"
+// const Schema = mongoose.Schema
+// const annonceSchema = new Schema({
+//   title : { type: String, required: true },
+//   type : ,
+//   address: [{
+//     building: { type : String, default : '' },
+//     street: { type : String, default : '' },
+//     zipcode: { type : String, default : '' },
+//     coord: [{ type : Number, default : 0 }],
+//   }],
+//
+//
+// })
+//
+// let Annonce = mongoose.model('annonce', annonceSchema);
+// module.exports = Annonce;
