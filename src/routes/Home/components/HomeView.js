00da4639f594df429e97bd978ad79d7dcf461ec4
lealivran <lealivran@msn.com>
2016-06-12 19:20:19 +0200

    create annonce component

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,10 +1,19 @@
 import React from 'react' 
 import CityPresentation from '../../../components/cityPresentation'
+import Annonce from '../../../components/Annonce'
+
 import classes from './HomeView.scss'
 
 export const HomeView = () => (
   <div>
     <CityPresentation/>
+
+    <div className={classes.annonceContainer}>
+      <Annonce/>
+      <Annonce/>
+      <Annonce/>
+    </div>
+    
   </div>
 )
 
