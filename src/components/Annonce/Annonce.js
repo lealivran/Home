0f2fdb641193fe76882abfdae64df8ad5feedddb
lealivran <lealivran@msn.com>
2016-06-12 19:20:19 +0200

    create annonce component

--- src/components/Annonce/Annonce.js
+++ src/components/Annonce/Annonce.js
@@ -1,16 +1,20 @@
+
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import classes from './Annonce.scss'
 
 export const Annonce = () => (
-   <div className={classes.header}>
-     <div className={classes.first} >
-     </div>
-     <div className={classes.containerSecondThird}>
-       <div className={classes.second}></div>
-       <div className={classes.third}></div>
-     </div>
-   </div>
+
+  <div className={classes.annonce}>
+  <div className={classes.info}>
+    <p>Super maison de ouf
+    <span>1500€/mois</span>
+    </p>
+  </div>
+    <img className={classes.bgAnnonce} src="http://www.maisonsclairlogis.fr/wp-content/uploads/maison-moderne_semnoz_700.jpg" />
+
+  </div>
+
 )
 
 export default Annonce
