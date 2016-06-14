a5a321f5056f6766393f92364b2a05c49db70afc
Darry <darry.jdp@gmail.com>
2016-06-14 16:43:29 +0200

    add data

--- server/lib/api/annonces.js
+++ server/lib/api/annonces.js
@@ -24,6 +24,60 @@ exports.getAnnoncesByCity = function*(){
   }
   this.body = { data: annonces };
 }
+exports.getAnnonces = function*(){
+  if (!this.request.body) {
+    this.throw("The body is empty", 400);
+  }
+  if (!this.request.body.basic) {
+    this.throw("The body basic search is empty", 400);
+  }
+  if (!this.request.body.minPrice) {
+    this.throw("The body minPrice search is empty", 400);
+  }
+  if (!this.request.body.maxPrice) {
+    this.throw("The body maxPrice search is empty", 400);
+  }
+  if (!this.request.body.minArea) {
+    this.throw("The body minArea search is empty", 400);
+  }
+  if (!this.request.body.maxArea) {
+    this.throw("The body maxArea search is empty", 400);
+  }
+  if (!this.request.body.minStairs) {
+    this.throw("The body minStairs search is empty", 400);
+  }
+  if (!this.request.body.maxStairs) {
+    this.throw("The body maxStairs search is empty", 400);
+  }
+  if (!this.request.body.minBedroomNumber) {
+    this.throw("The body minBedroomNumber search is empty", 400);
+  }
+  if (!this.request.body.maxBedroomNumber) {
+    this.throw("The body maxBedroomNumber search is empty", 400);
+  }
+  if (!this.request.body.minRoomNumber) {
+    this.throw("The body minRoomNumber search is empty", 400);
+  }
+  if (!this.request.body.maxRoomNumber) {
+    this.throw("The body maxRoomNumber search is empty", 400);
+  }
+  //
+  let basicSearch = this.request.body.basic
+  basicSearch.price = { $lt: this.request.body.minPrice, $gt: this.request.body.maxPrice }
+  basicSearch.area = { $lt: this.request.body.minArea, $gt: this.request.body.maxArea }
+  basicSearch.features.stairs = { $lt: this.request.body.minStairs, $gt: this.request.body.maxStairs }
+  basicSearch.features.bedroomNumber = {
+    $lt: this.request.body.minBedroomNumber,
+    $gt: this.request.body.maxBedroomNumber
+  }
+  basicSearch.features.roomNumber = {
+    $lt: this.request.body.minRoomNumber ,
+    $gt: this.request.body.maxRoomNumber
+  }
+  let annonces = yield Annonce.find(basicSearch).exec();
+  this.body = {annonces:annonces }
+}
+
 exports.createAnnonce= function *() {
   console.log(this.request.body);
   if (!this.request.body) {
